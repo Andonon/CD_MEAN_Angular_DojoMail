06c88fba7a5948a92b5f0a9f9742feff1398cdea
Troy Center <troycenter1@gmail.com>
2017-07-14 18:08:33 -0700

    email is on the page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MailAppComponent } from './mail-app/mail-app.component';
+
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
 
 @NgModule({
   declarations: [
     AppComponent,
-    MailAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+ 		FormsModule, // <-- Include module in our AppModules
+    HttpModule // <-- Include module in our AppModules
   ],
   providers: [],
   bootstrap: [AppComponent]
